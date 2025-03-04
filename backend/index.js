be1e238dcbc6b14f5519be6c2a4868d04ee07ff2
patient36 <topfateson@gmail.com>
2025-03-04 18:47:46 +0200

    Feat: connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 5000
 dotenv.config();
 
+mongoose
+    .connect(process.env.MONGO_DB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.log(error.message))
+
 app.listen(PORT, () => {
-    console.log(`Server is ready on PORT :${PORT}`)
+    console.log(`Server is ready on PORT ${PORT}`)
 })
