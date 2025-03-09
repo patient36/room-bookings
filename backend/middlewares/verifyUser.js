975b015f1bb3fefb7f05c4a140ba74836e460147
patient36 <topfateson@gmail.com>
2025-03-09 22:53:34 +0200

    Feat: Added a verify user account type middleware

---
+++ backend/middlewares/verifyUser.js
@@ -0,0 +1,31 @@
+import User from "../models/user.model.js";
+
+export const isOwner = async (req, res, next) => {
+    const userId = req.query.user
+    const user = await User.findOne({ _id: userId, accountType: 'owner' })
+    if (!user) {
+        return res.status(401).json({ message: "User is a room owner" })
+    }
+    req.user = user
+    next()
+}
+
+export const isBooker = async (req, res, next) => {
+    const userId = req.query.user
+    const user = await User.findOne({ _id: userId, accountType: 'booker' })
+    if (!user) {
+        return res.status(401).json({ message: "User is not a booker" })
+    }
+    req.user = user
+    next()
+}
+
+export const isAdmin = async (req, res, next) => {
+    const userId = req.query.user
+    const user = await User.findOne({ _id: userId, accountType: 'admin' })
+    if (!user) {
+        return res.status(401).json({ message: "User is not a system Admin" })
+    }
+    req.user = user
+    next()
+}
