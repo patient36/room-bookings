3db9201e4d21962bb47b58509c387684b7da6047
patient36 <topfateson@gmail.com>
2025-03-11 16:57:42 +0200

    chore: added admin routes

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -9,6 +9,11 @@ import sendSMS from "../utils/sns.js"
 
 const adminRouter = express.Router()
 
+async function notifyUser(user, message, subject) {
+    await sendSMS(user.phone, message);
+    await sendEmail(user.email, message, subject);
+}
+
 // get all rooms
 adminRouter.get('/all-rooms', [protect, isAdmin], async (req, res, next) => {
     try {
@@ -45,6 +50,164 @@ adminRouter.get('/all-rooms', [protect, isAdmin], async (req, res, next) => {
             data: { rooms }
         })
 
+    } catch (error) {
+        next(error)
+    }
+})
+
+// get all bookings
+adminRouter.get('/all-bookings', [protect, isAdmin], async (req, res, next) => {
+    try {
+        const sort = req.query.sort || "createdAt"
+        const page = parseInt(req.query.page, 10) || 1
+        const limit = parseInt(req.query.limit, 10) || 10
+        const skip = (page - 1) * limit
+
+        const totalBookings = await Booking.countDocuments({})
+        const totalPages = Math.ceil(totalBookings / limit)
+
+        if (page > totalPages) {
+            return res.status(200).json({
+                meta: {
+                    totalPages,
+                    pageSize: 0,
+                    page,
+                    message: "Page not found",
+                },
+                data: {
+                    bookings: []
+                }
+            })
+        }
+
+        const bookings = await Booking.find({}).sort({ [sort]: -1 }).skip(skip).limit(limit)
+        res.status(200).json({
+            meta: {
+                sort,
+                totalPages,
+                pageSize: bookings.length,
+                page
+            },
+            data: { bookings }
+        })
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+// get all users
+adminRouter.get('/all-users', [protect, isAdmin], async (req, res, next) => {
+    try {
+        const sort = req.query.sort || "createdAt"
+        const page = parseInt(req.query.page, 10) || 1
+        const limit = parseInt(req.query.limit, 10) || 10
+        const skip = (page - 1) * limit
+
+        const totalUsers = await User.countDocuments({})
+        const totalPages = Math.ceil(totalUsers / limit)
+
+        if (page > totalPages) {
+            return res.status(200).json({
+                meta: {
+                    totalPages,
+                    pageSize: 0,
+                    page,
+                    message: "Page not found",
+                },
+                data: {
+                    users: []
+                }
+            })
+        }
+
+        const users = await User.find({}).sort({ [sort]: -1 }).skip(skip).limit(limit)
+        res.status(200).json({
+            meta: {
+                sort,
+                totalPages,
+                pageSize: users.length,
+                page
+            },
+            data: { users }
+        })
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+// activate room
+adminRouter.put('/activate-room', [protect, isAdmin], async (req, res, next) => {
+    try {
+        const roomId = req.body.roomId
+        const room = await Room.findOne({ _id: roomId, status: "inactive" }).populate("owner")
+        if (!room) {
+            return res.status(404).json({ message: "Room not found" })
+        }
+        await Room.updateOne({ _id: roomId }, { $set: { status: "active" } })
+
+        // notify room owner
+        const owner = room.owner
+        const text = `Dear ${owner.name}, we are pleased to inform you that your room ${room.name} has been activated and now it is open for booking. `
+        const subject = "Room activation"
+        await notifyUser(owner, text, subject)
+
+        res.status(200).json({ message: "Room activated successfully" })
+    } catch (error) {
+        next(error)
+    }
+})
+
+// deactivate room
+adminRouter.put('/deactivate-room', [protect, isAdmin], async (req, res, next) => {
+    try {
+        const roomId = req.body.roomId
+        const room = await Room.findOne({ _id: roomId, status: "active" }).populate("owner")
+        if (!room) {
+            return res.status(404).json({ message: "Room not found" })
+        }
+        await Room.updateOne({ _id: roomId }, { $set: { status: "inactive" } })
+
+        // notify owner
+        const owner = room.owner
+        const text = `Dear ${owner.name}, we are sorry to inform you that your room ${room.name} has been temporarily deactivated and  it will be closed for booking until re-activation. `
+        const subject = "Room de-activation"
+        await notifyUser(owner, text, subject)
+
+        res.status(200).json({ message: "Room deactivated successfully" })
+    } catch (error) {
+        next(error)
+    }
+})
+
+// revoke user room owner access
+adminRouter.put('/revoke-owner', [protect, isAdmin], async (req, res, next) => {
+    try {
+        const userId = req.body.userId
+        const user = await User.findOne({ _id: userId, accountType: "owner" })
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        await User.updateOne({ _id: userId }, { $set: { accountType: "booker" } })
+
+        // notify user
+        res.status(200).json({ message: "Room owner access revoked successfully" })
+    } catch (error) {
+        next(error)
+    }
+})
+
+// grant user room owner access
+adminRouter.put('/grant-owner', [protect, isAdmin], async (req, res, next) => {
+    try {
+        const userId = req.body.userId
+        const user = await User.findOne({ _id: userId, accountType: "booker" })
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        await User.updateOne({ _id: userId }, { $set: { accountType: "owner" } })
+        res.status(200).json({ message: "Room owner access granted successfully" })
     } catch (error) {
         next(error)
     }
